a79cf929dbc99439faf67a0fc5233993a9908f0f
David FRANCK <davfranck@gmail.com>
2023-12-13 06:50:14 +0100

    Day 2 - improvements after reading other solutions

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -1,26 +1,36 @@
 package games;
 
 public class FizzBuzz {
+
+    public static final int BUZZ = 5;
+    public static final int FIZZ = 3;
+    public static final int MIN_RANGE = 0;
+    public static final int MAX_RANGE = 100;
+
     private FizzBuzz() {
     }
 
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input <= 0 || input > 100) {
+        if (isOutOfRange(input)) {
             throw new OutOfRangeException();
         }
-        if (isMultipleOf(3, input) && isMultipleOf(5, input)) {
+        if (is(FIZZ, input) && is(BUZZ, input)) {
             return "FizzBuzz";
         }
-        if (isMultipleOf(3, input)) {
+        if (is(FIZZ, input)) {
             return "Fizz";
         }
-        if (isMultipleOf(5, input)) {
+        if (is(BUZZ, input)) {
             return "Buzz";
         }
         return input.toString();
     }
 
-    private static boolean isMultipleOf(int multiple, Integer input) {
+    private static boolean isOutOfRange(Integer input) {
+        return input <= MIN_RANGE || input > MAX_RANGE;
+    }
+
+    private static boolean is(int multiple, Integer input) {
         return input % multiple == 0;
     }
 
